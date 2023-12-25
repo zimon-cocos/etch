c70a6c9fc0a18196a5ef019e24ad653b01755674
Šimon R <111566187+zimon-cocos@users.noreply.github.com>
2023-12-25 19:25:20 +0100

    arrange divs in grid, add eventlistener for each

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const canvasBox = document.getElementById("canvas")
+const canvas = document.getElementById("canvas")
 
 
-for(let i = 0; i>16 ;i++){
-    canvasBox.appendChild("div")
+
+
+for(let i = 0; i< 16 ;i++){
+    newdiv = document.createElement("div")
+    newdiv.classList.add("gamecube")
+    newdiv.innerText = "AB"
+    canvas.appendChild(newdiv)
     console.log("retard")
 }
+
+const gamecubes = document.querySelectorAll(".gamecube")
+
+for(let i = 0; i<gamecubes.length; i++){
+    gamecubes[i].addEventListener("mouseover",e=>{
+       console.log("hovered")
+    })
+}
+
+
+
+
+
+
+
