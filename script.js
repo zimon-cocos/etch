4e4ee87239bdd3f0b32a2994ca4ebe380a7f66c0
Šimon R <111566187+zimon-cocos@users.noreply.github.com>
2023-12-29 18:51:26 +0100

    add button for squares amount
    I have not managed to make the button keep repeating the prompt if you
    choose more than 100 squares also, the squares do not get a new event
    lisener after they get generated using the button.

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const canvas = document.getElementById("canvas")
+const amountBtn = document.getElementById("amountBtn")
 
-
-
-
-for(let i = 0; i< 16 ;i++){
+function squaresmaker(boxAmount){
+    for(let i = 0; i< boxAmount ;i++){
     newdiv = document.createElement("div")
     newdiv.classList.add("gamecube")
     newdiv.innerText = "AB"
     canvas.appendChild(newdiv)
 
 }
+}
+
+squaresmaker(16)
+
+amountBtn.addEventListener("click",()=>{
+    let boxAmount = parseInt(prompt("Kolko chces stvorcov? Menej ako 100 prosim"))
+    if(boxAmount>100){
+        const boxAmount = parseInt(prompt("Kolko chces stvorcov? Menej ako 100 prosim"))
+    }
+    console.log("chces " + boxAmount + " stvorcov")
+    const forDeletion = document.querySelectorAll(".gamecube").forEach(forDeletion => {
+        forDeletion.remove()
+    });
+    squaresmaker(boxAmount)
+    
+
+})
 
 const gamecubes = document.querySelectorAll(".gamecube")
 
